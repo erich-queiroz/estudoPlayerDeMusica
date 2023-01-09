274ea307fc76d78defd2e497244803f7556a983a
Erich Queiroz <95364468+erich-queiroz@users.noreply.github.com>
2023-01-09 19:58:27 -0300

    Add "dados.json"
    Agora os nomes das músicas são exibidos via requisição JSON.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,19 @@
 //alert('testando');
+// **************
+// Obtendo dados de musica e banda, através do arquivo "dados.json"
+//
+//const nomeMusica = document.getElementById('musica-nome');
+//nomeMusica.innerText = 'A visit from the Dead';
+//
+//const nomeBanda = document.getElementById('banda-nome');
+//nomeBanda.innerText = 'King Diamond';
+//musica.play();
 
-const nomeMusica = document.getElementById('musica-nome');
-nomeMusica.innerText = 'A visit from the Dead';
 
 const play = document.getElementById('play'); // botão de play
 const musica = document.getElementById('musica'); // captura da tag audio
 
-const nomeBanda = document.getElementById('banda-nome');
-nomeBanda.innerText = 'King Diamond';
-//musica.play();
+
 
 function playMusic() {
     musica.play();
@@ -28,3 +33,19 @@ function curtirMusica() {
 }
 like.addEventListener('click', curtirMusica);
 
+//let dadosMusica = document.getElementById('info-musica');
+    //fetch("../dados.json").then((Response) => {}
+
+let dadosMusica = document.querySelector("#musica-nome");
+let dadosBanda = document.querySelector("#banda-nome");
+        fetch("../dados.json").then((response) => {
+            response.json().then((dados) => {
+                dados.musicas.map((musica) => {
+                    console.log(musica.musica);
+                    dadosMusica.innerHTML = musica.musica
+                    dadosBanda.innerHTML = musica.banda
+                })
+                
+            } )
+        }
+)
