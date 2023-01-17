490138b0da578f0d5fca3e17509c5bf489be27da
guilhermehub12 <guilhermee1312@hotmail.com>
2023-01-16 21:35:02 -0300

    feat: pause function and updatemusic progress bar

--- js/app.js
+++ js/app.js
@@ -11,25 +11,31 @@
 
 
 const play = document.getElementById('play'); // botão de play
+const pause = document.getElementById('pause'); //botão de pause
 const musica = document.getElementById('musica'); // captura da tag audio
 
-
+play.addEventListener('click', playMusic);
+pause.addEventListener('click', pauseMusic);
 
 function playMusic() {
     musica.play();
-    play.innerHTML = `<i class="bi bi-pause-circle-fill"></i>`;
+    document.querySelector("#pause").style.display = "block";
+  document.querySelector("#play").style.display = "none";
 }
+
 function pauseMusic() {
     musica.pause();
+    document.querySelector("#pause").style.display = "none";
+  document.querySelector("#play").style.display = "block";
 }
-play.addEventListener('click', playMusic);
+
 //play.addEventListener('dblclick', pauseMusic);
 
 const like = document.getElementById('curtir');
 
 function curtirMusica() {
     
-    like.innerHTML = `<i class="bi bi-heart-fill"></i>`;
+    like.innerHTML = `<i class="bi bi-heart-fill"></i>`
 }
 like.addEventListener('click', curtirMusica);
 
@@ -49,3 +55,15 @@ let dadosBanda = document.querySelector("#banda-nome");
             } )
         }
 )
+//aqui eu pego a varíavel musica e adiciono um evento para pegar a 
+musica.addEventListener("timeupdate", atualizarBarra);
+
+
+function atualizarBarra() {
+    // a varíavel barra representa o progresso atual
+    let barra = document.querySelector("#progresso-atual");
+    //aqui eu pego o tempo da música ao decorrer que ela vai tocando que está em segundos e divido pela duração total que também está em segundos e depois divido por 100 para transformar em número real e com o Math.floor faz com que o resultado arredonde para baixo virando um valor inteiro( ex: 3.40 = 3) e concateno com % para ir no estilo da barra que é o tamanho (width)
+    barra.style.width =
+      Math.floor((musica.currentTime / musica.duration) * 100) + "%";
+
+  }
